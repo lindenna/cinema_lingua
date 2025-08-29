a11c6b37ad3c1a7130f12f50afb2d05cdd0ff70f
natali.lindenmeyer <natali.lindenmeyer@gmail.com>
2025-08-29 16:07:33 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Cinema Lingua
+
+Cinema Lingua is a web application designed to help users learn languages through movies. This platform allows users to find movies for a target language and country. Additionally, the user's search can be further refined by including preferred movie provider and genres. 
+
+## Table of contents
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+
+## My process
+
+### Built with
+
+- HTML and CSS for frontend development
+- Python with Flask framework for backend development
+- TMDB API for movie data
+- Bootstrap for responsive design and styling
+- Jinja2 for template rendering
+- RESTful API principles
+- Git for version control
+
+### What I learned
+
+Through this project, I learned how to retrieve data from an API and integrate it into a web app. I also learned how to use Flask for the back-end features of a web app. Another new concept that I learned through this project is pagination.
+
+### Continued development
+
+Going forward, I'd like to expand my use of CSS and Bootstrap on this web app to make it more visually appealing. I also plan on adding the option to search for movies or tv shows. 
+
+### Useful resources
+
+- [How to Create a Python Web App with Flask](https://www.sourish.dev/blog/tutorials/flask.html) - This article helped me learn how to build a web app using Flask and an API. I'd recommend it to anyone still learning this concept.
+- [Pagination in Flask](https://www.youtube.com/watch?v=U18hO1ngZEQ) - This is a great video that gave me an idea of how to set up pagination in Flask.
---
+++ main.py
@@ -0,0 +1,159 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+import os
+from dotenv import load_dotenv
+
+import requests
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    if "error" in request.args:
+        return render_template("index.html", error=request.args["error"])
+    return render_template("index.html")
+
+
+@app.route("/info", methods=["GET", "POST"])
+def info():
+    # if request.method == "GET":
+    #     return redirect("/")  # Redirects user back to root page
+    if request.method == "GET":
+        # Get page number from query params, default to 1
+        page = request.args.get("page", default=1, type=int)
+        country = request.args.get("country")
+        language = request.args.get("language")
+        provider = request.args.get("provider")
+    else:
+        form_data = request.form
+        country = form_data["country"]
+        language = form_data["language"]
+        provider = form_data["provider"]
+        page = 1
+
+    headers = {"accept": "application/json", "Authorization": f"Bearer {TOKEN}"}
+
+    countries_url = "https://api.themoviedb.org/3/configuration/countries"
+    countries = requests.get(countries_url, headers=headers).json()
+    country_item = next(
+        (
+            item
+            for item in countries
+            if item["english_name"].lower() == country.lower()
+            or item["native_name"].lower() == country.lower()
+        ),
+        None,
+    )
+
+    if not country_item:
+        return redirect(url_for("index", error="Country not found"))
+    country_name = country_item["iso_3166_1"]
+
+    languages_url = "https://api.themoviedb.org/3/configuration/languages"
+    languages = requests.get(languages_url, headers=headers).json()
+    language_item = next(
+        (
+            item
+            for item in languages
+            if item["english_name"].lower() == language.lower()
+        ),
+        None,
+    )
+
+    if not language_item:
+        return redirect(url_for("index", error="Language not found"))
+    language_name = language_item["iso_639_1"]
+
+    if provider:
+        providers_url = "https://api.themoviedb.org/3/watch/providers/movie"
+        providers = requests.get(providers_url, headers=headers).json()
+        provider_item = next(
+            (
+                item
+                for item in providers["results"]
+                if item["provider_name"].lower() == provider.lower()
+            ),
+            None,
+        )
+
+        if not provider_item:
+            return redirect(url_for("index", error="Provider not found"))
+
+        provider_id = provider_item["provider_id"]
+
+    genres_url = "https://api.themoviedb.org/3/genre/movie/list"
+    genres = requests.get(genres_url, headers=headers).json()
+    selected_genres = request.form.getlist("genres")
+    for genre in selected_genres:
+        genre_item = next(
+            (item for item in genres["genres"] if item["name"].lower() == genre), None
+        )
+        if genre_item:
+            genre_id = genre_item["id"]
+            if "with_genres" in locals():
+                with_genres += f",{genre_id}"
+            else:
+                with_genres = str(genre_id)
+
+    if not provider and "with_genres" not in locals():
+        url = f"https://api.themoviedb.org/3/discover/movie?with_origin_country={country_name}&with_original_language={language_name}&page={page}"
+    elif not provider and "with_genres" in locals():
+        url = f"https://api.themoviedb.org/3/discover/movie?with_origin_country={country_name}&with_original_language={language_name}&with_genres={with_genres}&page={page}"
+    elif provider and "with_genres" not in locals():
+        url = f"https://api.themoviedb.org/3/discover/movie?with_origin_country={country_name}&with_original_language={language_name}&with_watch_providers={provider_id}&watch_region=US&page={page}"
+    elif provider and "with_genres" in locals():
+        url = f"https://api.themoviedb.org/3/discover/movie?with_origin_country={country_name}&with_original_language={language_name}&with_watch_providers={provider_id}&watch_region=US&with_genres={with_genres}&page={page}"
+
+    movie_data = requests.get(url, headers=headers).json()
+
+    poster_paths = []
+    titles = []
+    release_dates = []
+    overviews = []
+
+    for movie in movie_data["results"]:
+
+        if movie["poster_path"] is None:
+            poster_paths.append(None)
+        else:
+            poster_paths.append(
+                "https://image.tmdb.org/t/p/w500" + movie["poster_path"]
+            )
+
+        title = movie["title"]
+        titles.append(title)
+
+        release_date = movie["release_date"]
+        date_items = release_date.split("-")
+        if len(date_items) == 3:
+            release_date = f"{date_items[1]}/{date_items[2]}/{date_items[0]}"
+        release_dates.append(release_date)
+
+        overview = movie["overview"]
+        overviews.append(overview)
+
+    total_pages = movie_data.get("total_pages", 1)
+
+    return render_template(
+        "info.html",
+        titles=titles,
+        release_dates=release_dates,
+        overviews=overviews,
+        poster_paths=poster_paths,
+        info={
+            "country": country.capitalize(),
+            "language": language.capitalize(),
+            "genres": selected_genres,
+            "provider": provider.capitalize() if provider else None,
+            "page": page,
+            "total_pages": total_pages,
+        },
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5500, debug=True)
